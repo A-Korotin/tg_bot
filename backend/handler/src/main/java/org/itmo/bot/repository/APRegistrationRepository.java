5c6a4a0322608384b8569ac96ccff10e04446de0
Alexey Korotin <sx12fx@gmail.com>
2023-09-10 16:43:51 +0300

    fix: APRegistrationRepository generics issue

--- backend/handler/src/main/java/org/itmo/bot/repository/APRegistrationRepository.java
+++ backend/handler/src/main/java/org/itmo/bot/repository/APRegistrationRepository.java
@@ -1,8 +1,9 @@
 package org.itmo.bot.repository;
 
+import org.itmo.bot.model.APRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface APRegistrationRepository extends JpaRepository<APRegistrationRepository, Long> {
+public interface APRegistrationRepository extends JpaRepository<APRegistration, Long> {
 }
