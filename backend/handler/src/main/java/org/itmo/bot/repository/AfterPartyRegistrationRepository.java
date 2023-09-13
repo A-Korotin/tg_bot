31e0717b17aacd3a2590135eca0fcb7108c56af5
AlexeyLush <alexeylush4060@gmail.com>
2023-09-13 10:19:30 +0300

    fix: removed unused imports

--- backend/handler/src/main/java/org/itmo/bot/repository/AfterPartyRegistrationRepository.java
+++ backend/handler/src/main/java/org/itmo/bot/repository/AfterPartyRegistrationRepository.java
@@ -1,15 +1,8 @@
 package org.itmo.bot.repository;
 
-import org.apache.kafka.common.quota.ClientQuotaAlteration;
 import org.itmo.bot.model.AfterPartyRegistration;
-import org.itmo.bot.model.Student;
-import org.itmo.bot.state.Conversation;
-import org.itmo.bot.state.StateName;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
