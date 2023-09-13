d0c50e4bf67155cde5767798fcc2aacef673e6ea
AlexeyLush <alexeylush4060@gmail.com>
2023-09-13 10:18:27 +0300

    feat: added KafkaListener for message with photo

--- backend/handler/src/main/java/org/itmo/bot/service/HandlerService.java
+++ backend/handler/src/main/java/org/itmo/bot/service/HandlerService.java
@@ -2,6 +2,7 @@ package org.itmo.bot.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.itmo.bot.common.dto.PhotoMessageDTO;
 import org.itmo.bot.common.dto.TextMessageDTO;
 import org.itmo.bot.common.dto.TextResponseDTO;
 import org.itmo.bot.common.topics.Topic;
@@ -24,6 +25,16 @@ public class HandlerService {
     @KafkaListener(topics = Topic.TEXT_MESSAGE_TOPIC, groupId = "my consumer")
     public void consume(TextMessageDTO dto) {
         log.info("Consumer consume Kafka message -> {}", dto.getMessage());
+        Conversation conversation = conversationService.loadByChatId(dto.getChatId(), dto.getNickName());
+        conversation.setStateFactory(factory);
+        TextResponseDTO responseDTO = conversation.receive(dto);
+        conversationService.update(conversation.getStateName(), conversation.getChatId());
+        template.send(Topic.TEXT_RESPONSE_TOPIC, responseDTO);
+    }
+
+    @KafkaListener(topics = Topic.PHOTO_MESSAGE_TOPIC, groupId = "my consumer")
+    public void consumePhoto(PhotoMessageDTO dto) {
+        log.info("Consumer consume Kafka photo -> {}", dto.getPhotoId());
         Conversation conversation = conversationService.loadByChatId(dto.getChatId(), dto.getNickName());
         conversation.setStateFactory(factory);
         TextResponseDTO responseDTO = conversation.receive(dto);
