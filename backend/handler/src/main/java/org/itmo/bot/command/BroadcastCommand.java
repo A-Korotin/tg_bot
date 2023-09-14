64fd1abc32fb441d3b5c6234ff5b6ecff4d961c9
Alexey Korotin <sx12fx@gmail.com>
2023-09-14 13:22:48 +0300

    feat: introducing broadcast admin command

---
+++ backend/handler/src/main/java/org/itmo/bot/command/BroadcastCommand.java
@@ -0,0 +1,30 @@
+package org.itmo.bot.command;
+
+import lombok.RequiredArgsConstructor;
+import org.itmo.bot.common.dto.TextResponseDTO;
+import org.itmo.bot.common.topics.Topic;
+import org.itmo.bot.exception.command.InvalidArgumentsException;
+import org.itmo.bot.service.StudentService;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BroadcastCommand extends Command {
+    private final StudentService studentService;
+    private final KafkaTemplate<String, TextResponseDTO> template;
+
+    @Override
+    public void execute(long chatId) {
+        if (args.length != 2) {
+            throw new InvalidArgumentsException("К этой команде ожидается аргумент через пробел");
+        }
+        String message = args[1];
+
+        studentService.findAllRegistered().forEach(s ->
+                template.send(Topic.TEXT_RESPONSE_TOPIC, TextResponseDTO.builder()
+                        .chatId(s.getConversation().getChatId())
+                        .message(message)
+                        .build()));
+    }
+}
