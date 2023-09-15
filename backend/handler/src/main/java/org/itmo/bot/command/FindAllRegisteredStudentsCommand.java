eb716fa2f8c56dce47ac909e2d428bc7dfc9c3e9
Alexey Korotin <sx12fx@gmail.com>
2023-09-15 12:36:56 +0300

    feat: CSV registered student export UTF-8 with BOM file format

--- backend/handler/src/main/java/org/itmo/bot/command/FindAllRegisteredStudentsCommand.java
+++ backend/handler/src/main/java/org/itmo/bot/command/FindAllRegisteredStudentsCommand.java
@@ -1,45 +1,41 @@
 package org.itmo.bot.command;
 
 import lombok.RequiredArgsConstructor;
-import org.itmo.bot.common.dto.TextResponseDTO;
+import org.itmo.bot.common.dto.FileResponseDTO;
 import org.itmo.bot.common.topics.Topic;
-import org.itmo.bot.model.Representable;
+import org.itmo.bot.model.CSVRepresentable;
 import org.itmo.bot.model.Student;
 import org.itmo.bot.service.StudentService;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.StreamSupport;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
 public class FindAllRegisteredStudentsCommand extends Command {
     private final StudentService studentService;
-    private final KafkaTemplate<String, TextResponseDTO> template;
+    private final KafkaTemplate<String, FileResponseDTO> template;
 
     @Override
     public void execute(long chatId) {
         Iterable<Student> students = studentService.findAllRegistered();
 
-        List<String> representations = StreamSupport
-                .stream(students.spliterator(), false)
-                .map(Representable::represent)
-                .toList();
-
         StringBuilder builder = new StringBuilder();
-        for (int i = 1; i <= representations.size(); ++i) {
-            builder.append(i).append(": ").append(representations.get(i - 1)).append('\n');
-        }
-        if (builder.isEmpty()) {
-            builder.append("Пока никого :(");
+        builder.append('\ufeff'); // BOM for Excel UTF-8 encoding
+        builder.append("Имя;Фамилия;Группа;Номер ИСУ;Ник телеграмм\n");
+        for (CSVRepresentable representable: students) {
+            builder.append(representable.representAsCSVRecord()).append('\n');
         }
 
-        TextResponseDTO textResponseDTO = TextResponseDTO.builder()
+
+        FileResponseDTO dto = FileResponseDTO.builder()
                 .chatId(chatId)
-                .message(builder.toString())
+                .caption("Лови!")
+                .fileName("registrations.csv")
+                .content(builder.toString().getBytes(StandardCharsets.UTF_8))
                 .build();
 
-        template.send(Topic.TEXT_RESPONSE_TOPIC, textResponseDTO);
+        template.send(Topic.FILE_RESPONSE_TOPIC, dto);
     }
 }
